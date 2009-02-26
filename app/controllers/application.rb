074d994b54ddd701dcf31487921e83d56c11432f
Stuart Eccles <stueccles@stuart-eccless-macbook-pro-3.local>
2009-02-26 16:38:04 +0000

    added tutorial app

---
+++ app/controllers/application.rb
@@ -0,0 +1,21 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  layout 'index'
+  before_filter :set_facebook_session
+  helper_method :facebook_session
+  
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '482899c303b35f2c5f66a8aef1d97c3c'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+end
